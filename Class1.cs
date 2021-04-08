e7852c0803f605705ed473aa1f4064da713ac65d
Nestor Calvo <51687105+nestorcalvo@users.noreply.github.com>
2021-04-07 22:27:14 -0500

    Add files via upload
    Add initial files

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Windows.Forms;
+
+namespace VirtualMachineDetection
+{
+	public class Detection
+	{
+		public void Run()
+		{
+			string os_query = "SELECT * FROM Win32_OperatingSystem";
+			ManagementObjectSearcher os_searcher = new ManagementObjectSearcher(os_query);
+			foreach (ManagementObject info in os_searcher.Get())
+			{
+				Console.WriteLine(info.Properties["Name"].Value.ToString());
+			}
+
+			MessageBox.Show("This computer is in a virtual enviroment");
+		}
+
+	}
+}
